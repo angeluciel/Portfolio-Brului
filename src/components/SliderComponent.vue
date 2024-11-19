6baf272771210a19efd68594f87f1288d3763530
Luciel <jpmizidoro@gmail.com>
2024-11-19 13:20:30 -0300

    SLIDER
    criando o slider

---
+++ src/components/SliderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="slide">
+      <div class="item"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@use "../assets/variables.scss" as var;
+
+.item {
+  background-image: url(../assets/images/wallpaper1.png);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,7 +21,8 @@
       </div>
       <div class="bottom">
         <div class="socials">
-          <button class="cta_button" type="button">
+          <!-- <button class="cta_button" type="button">
+          
             <span>Bring your visions to life</span>
             <Icon
               icon="ic:baseline-arrow-forward-ios"
@@ -30,11 +31,24 @@
               style="color: white"
             />
           </button>
-          <div class="instagram">
-            <fa :icon="['fab', 'instagram']" />
+           -->
+          <div id="container">
+            <button class="learn-more">
+              <span class="circle" aria-hidden="true">
+                <span class="icon arrow"></span>
+              </span>
+              <span class="button-text">Bring your visions to life</span>
+            </button>
           </div>
-          <div class="twitter">
-            <fa :icon="['fab', 'x-twitter']" />
+          <div class="socials">
+            <div class="instagram slide">
+              <!-- Adiciona a classe 'slide' -->
+              <fa :icon="['fab', 'instagram']" />
+            </div>
+            <div class="twitter slide">
+              <!-- Adiciona a classe 'slide' -->
+              <fa :icon="['fab', 'x-twitter']" />
+            </div>
           </div>
         </div>
         <div class="stats">
@@ -60,16 +74,32 @@
       </div>
     </div>
   </div>
-  <div class="lower"></div>
+
+  <div class="lower">
+    <SliderComponent />
+  </div>
 </template>
 
 <script setup>
 import HeaderBar from "../components/HeaderBar.vue";
+import SliderComponent from "@/components/SliderComponent.vue";
 import { Icon } from "@iconify/vue";
 </script>
 
 <style lang="scss" scoped>
 @use "../assets/variables.scss" as var;
+
+$bg: #f3f8fa;
+$white: #fff;
+$black: #282936;
+
+@mixin transition(
+  $property: all,
+  $duration: 0.45s,
+  $ease: cubic-bezier(0.65, 0, 0.076, 1)
+) {
+  transition: $property $duration $ease;
+}
 
 .body {
   display: flex;
@@ -151,6 +181,7 @@ import { Icon } from "@iconify/vue";
         align-items: center;
         gap: 2rem;
 
+        /*
         .cta_button {
           display: flex;
           justify-content: center;
@@ -162,9 +193,15 @@ import { Icon } from "@iconify/vue";
           background: var.$main-button-bg;
           color: white;
           font-style: italic;
+          transition: 0.5s ease;
+          cursor: pointer;
 
+          &:hover {
+            background: var.$main-button-bg-hover;
+          }
           span {
             font-size: 30px;
+            font-style: italic;
           }
 
           .greater-than {
@@ -172,17 +209,141 @@ import { Icon } from "@iconify/vue";
             width: 36px;
           }
         }
+          */
 
-        .instagram,
-        .twitter {
+        button {
+          position: relative;
+          display: inline-block;
+          cursor: pointer;
+          outline: none;
+          border: 0;
+          vertical-align: middle;
+          text-decoration: none;
+          background: transparent;
+          padding: 0;
+          font-size: inherit;
+          font-family: inherit;
+          &.learn-more {
+            width: 20rem;
+            height: auto;
+            .circle {
+              @include transition(all, 0.45s, cubic-bezier(0.65, 0, 0.076, 1));
+              position: relative;
+              display: block;
+              margin: 0;
+              width: 3rem;
+              height: 3rem;
+              background: $black;
+              border-radius: 1.625rem;
+              .icon {
+                @include transition(
+                  all,
+                  0.45s,
+                  cubic-bezier(0.65, 0, 0.076, 1)
+                );
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                margin: auto;
+                background: $white;
+                &.arrow {
+                  @include transition(
+                    all,
+                    0.45s,
+                    cubic-bezier(0.65, 0, 0.076, 1)
+                  );
+                  left: 0.625rem;
+                  width: 1.125rem;
+                  height: 0.125rem;
+                  background: none;
+                  &::before {
+                    position: absolute;
+                    content: "";
+                    top: -0.25rem;
+                    right: 0.0625rem;
+                    width: 0.625rem;
+                    height: 0.625rem;
+                    border-top: 0.125rem solid #fff;
+                    border-right: 0.125rem solid #fff;
+                    transform: rotate(45deg);
+                  }
+                }
+              }
+            }
+            .button-text {
+              @include transition(all, 0.45s, cubic-bezier(0.65, 0, 0.076, 1));
+
+              font-size: 1rem;
+              line-height: 1.5;
+              position: absolute;
+              top: 0;
+              left: 0;
+              right: 0;
+              bottom: 0;
+              padding: 0.75rem 0;
+              margin: 0 0 0 1.85rem;
+              color: $black;
+              font-weight: 700;
+              line-height: 1.6;
+              text-align: center;
+              text-transform: uppercase;
+            }
+          }
+          &:hover {
+            .circle {
+              width: 100%;
+              .icon {
+                &.arrow {
+                  background: $white;
+                  transform: translate(1rem, 0);
+                }
+              }
+            }
+            .button-text {
+              color: $white;
+            }
+          }
+        }
+
+        @supports (display: grid) {
+          body {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-gap: 0.625rem;
+            grid-template-areas: ". main main ." ". main main .";
+          }
+
+          #container {
+            grid-area: main;
+            align-self: center;
+            justify-self: center;
+          }
+        }
+
+        .slide {
           display: flex;
           justify-content: center;
           align-items: center;
-          padding: 0;
-          height: 56px;
           width: 56px;
+          height: 56px;
           border-radius: 100%;
-          border: 3px solid var.$text-color;
+          background-color: transparent;
+          border: 2px solid var(--color);
+          color: var(--color);
+          transition: 0.25s;
+          box-shadow: inset 0 0 0 0 var(--hover);
+
+          &:hover,
+          &:focus {
+            box-shadow: inset 6.5em 0 0 0 var(--hover);
+            color: #fff;
+            border-color: var(--hover);
+          }
+
+          svg {
+            width: 24px;
+            height: 24px;
+          }
         }
       }
 
