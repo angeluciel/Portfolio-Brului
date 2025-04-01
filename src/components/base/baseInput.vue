517461c630c9a6a8f86835d47fef9911f118a1a1
Luciel <jpmizidoro@gmail.com>
2025-04-01 18:37:00 -0300

    input component

---
+++ src/components/base/baseInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- INPUT COMPONENT-->
+    <span>{{ title }}</span>
+    <div>
+      <!-- BOX -->
+      <Icon v-if="icon" :icon="icon" class="mr-2" />
+      <input :placeholder="placeholder" :type="type" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { Icon } from "@iconify/vue";
+
+const props = defineProps({
+  variant: { type: String, default: "login" },
+  icon: String,
+  title: String,
+  placeholder: String,
+  type: { type: String, default: "Text" },
+});
+</script>
+
+<style scoped></style>
