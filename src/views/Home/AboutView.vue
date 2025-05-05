e81067f39e4a0c9339c8a8ffe07d6ed51789b68f
Luciel <jpmizidoro@gmail.com>
2025-05-05 00:26:26 -0300

    fix: scroll on all pages (was the footer's fault), still not 100% but works if using w-full only

---
+++ src/views/Home/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-[200dvh] w-full bg-violet-50 overflow-x-hidden"></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/PricesView.vue
+++ src/views/PricesView.vue
@@ -72,110 +72,101 @@ const showButtons = ref(false);
 </script>
 
 <template>
-  <main>
-    <HeaderBar_mobile />
+  <HeaderBar_mobile />
 
-    <div class="flex flex-col gap-4 w-dvw !py-24 bg-violet-50">
+  <div class="flex flex-col gap-4 w-full !py-24 bg-violet-50 overflow-x-hidden">
+    <div
+      class="relative flex flex-col justify-between gap-4 items-center w-full !pl-6 sm:!px-24"
+    >
       <div
-        class="relative flex flex-col justify-between gap-4 items-center w-full !pl-6 sm:!px-24"
+        class="pointer-events-none absolute inset-y-0 left-0 w-8 bg-gradient-to-r from-white to-transparent"
+      ></div>
+      <div
+        class="pointer-events-none absolute inset-y-0 right-0 w-8 bg-gradient-to-l from-white to-transparent"
+      ></div>
+
+      <div
+        class="flex flex-col sm:flex-row gap-8 sm:justify-between justify-center w-full"
       >
-        <div
-          class="pointer-events-none absolute inset-y-0 left-0 w-8 bg-gradient-to-r from-white to-transparent"
-        ></div>
-        <div
-          class="pointer-events-none absolute inset-y-0 right-0 w-8 bg-gradient-to-l from-white to-transparent"
-        ></div>
-
-        <div
-          class="flex flex-col sm:flex-row gap-8 sm:justify-between justify-center w-full"
-        >
-          <h1 class="!font-abril !text-4xl text-charcoal sm:!text-5xl">
-            Place your Order
-          </h1>
-          <div class="flex flex-col items-start">
-            <span>Need something that's not on the menu?</span>
-            <div class="flex text-gold-500">
-              <span>Let me know how I can help!</span>
-              <Icon icon="ri:arrow-right-up-line" width="24" height="24" />
-            </div>
-          </div>
-        </div>
-
-        <div
-          class="flex justify-around w-full [&>*]:w-full text-center overflow-x-scroll no-scrollbar gap-12 sm:gap-0"
-        >
-          <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
-            All Prices
-          </div>
-          <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
-            Complete Render
-          </div>
-          <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
-            Lineart + Base Color
-          </div>
-          <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
-            Emotes
-          </div>
-          <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
-            Background
+        <h1 class="!font-abril !text-4xl text-charcoal sm:!text-5xl">
+          Place your Order
+        </h1>
+        <div class="flex flex-col items-start">
+          <span>Need something that's not on the menu?</span>
+          <div class="flex text-gold-500">
+            <span>Let me know how I can help!</span>
+            <Icon icon="ri:arrow-right-up-line" width="24" height="24" />
           </div>
         </div>
       </div>
 
       <div
-        class="relative flex-col w-dvw"
-        @mouseenter="showButtons = true"
-        @mouseleave="showButtons = false"
+        class="flex justify-around w-full [&>*]:w-full text-center overflow-x-scroll no-scrollbar gap-12 sm:gap-0"
       >
-        <!-- adicione relative aqui -->
-        <div class="flex w-full !pl-6 sm:!px-24 gap-2">
-          <span class="!font-abril text-2xl">All Services.</span>
-          <span class="!font-abril text-2xl text-gray-500">Choose Yours!</span>
+        <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
+          All Prices
         </div>
-
-        <div
-          class="flex justify-start w-full !p-6 sm:!px-24 gap-8 overflow-auto no-scrollbar snap-x snap-mandatory"
-          ref="scrollEl"
-        >
-          <CardTemplateVue
-            class="snap-center shrink-0 w-[250px]"
-            v-for="(card, index) in cards"
-            :key="index"
-            v-bind="card"
-          />
+        <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
+          Complete Render
         </div>
-
-        <!-- Mova os botões aqui, fora do scroll container -->
-        <button
-          @click="scrollLeft"
-          class="hidden md:flex absolute left-9 top-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.4)] text-gray-200 shadow-[0px_0px_16px_1px_rgba(255,255,255,0.5)] rounded-full p-2 transition-all duration-300"
-          :class="
-            showButtons
-              ? 'opacity-100 translate-y-0'
-              : 'opacity-0 translate-y-4'
-          "
-        >
-          <Icon icon="ri:arrow-left-s-line" width="24" height="24" />
-        </button>
-
-        <button
-          @click="scrollRight"
-          class="hidden md:flex absolute right-9 top-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.4)] text-gray-200 shadow-[0px_0px_8px_1px_rgba(89,125,161,0.5)] rounded-full p-2 transition-all duration-300"
-          :class="
-            showButtons
-              ? 'opacity-100 translate-y-0'
-              : 'opacity-0 translate-y-4'
-          "
-        >
-          <Icon icon="ri:arrow-right-s-line" width="24" height="24" />
-        </button>
+        <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
+          Lineart + Base Color
+        </div>
+        <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
+          Emotes
+        </div>
+        <div class="!p-1 sm:!py-3 sm:!px-5 hover:bg-gray-100 text-nowrap">
+          Background
+        </div>
       </div>
     </div>
-  </main>
 
-  <div>
-    <FooterBar />
+    <div
+      class="relative flex-col w-dvw"
+      @mouseenter="showButtons = true"
+      @mouseleave="showButtons = false"
+    >
+      <!-- adicione relative aqui -->
+      <div class="flex w-full !pl-6 sm:!px-24 gap-2">
+        <span class="!font-abril text-2xl">All Services.</span>
+        <span class="!font-abril text-2xl text-gray-500">Choose Yours!</span>
+      </div>
+
+      <div
+        class="flex justify-start w-full !p-6 sm:!px-24 gap-8 overflow-auto no-scrollbar snap-x snap-mandatory"
+        ref="scrollEl"
+      >
+        <CardTemplateVue
+          class="snap-center shrink-0 w-[250px]"
+          v-for="(card, index) in cards"
+          :key="index"
+          v-bind="card"
+        />
+      </div>
+
+      <!-- Mova os botões aqui, fora do scroll container -->
+      <button
+        @click="scrollLeft"
+        class="hidden md:flex absolute left-9 top-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.4)] text-gray-200 shadow-[0px_0px_16px_1px_rgba(255,255,255,0.5)] rounded-full p-2 transition-all duration-300"
+        :class="
+          showButtons ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+        "
+      >
+        <Icon icon="ri:arrow-left-s-line" width="24" height="24" />
+      </button>
+
+      <button
+        @click="scrollRight"
+        class="hidden md:flex absolute right-9 top-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.4)] text-gray-200 shadow-[0px_0px_8px_1px_rgba(89,125,161,0.5)] rounded-full p-2 transition-all duration-300"
+        :class="
+          showButtons ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+        "
+      >
+        <Icon icon="ri:arrow-right-s-line" width="24" height="24" />
+      </button>
+    </div>
   </div>
+  <FooterBar />
 </template>
 
 <style lang="scss" scoped></style>
