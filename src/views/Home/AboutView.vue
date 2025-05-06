c91ff6cce0c387fd7a81180e67e476fdb1947e74
Luciel <jpmizidoro@gmail.com>
2025-05-06 08:35:10 -0300

    I dont remember

--- src/views/Home/AboutView.vue
+++ src/views/Home/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="pt-80 h-[200dvh] w-full bg-violet-50 overflow-x-hidden">
-    <span class="home-heading !text-black" ref="buttonText">NEW HORIZONS</span>
+  <div
+    class="flex justify-center pt-80 h-[200dvh] w-full bg-violet-50 overflow-x-hidden"
+  >
+    <span class="home-heading !text-black" ref="buttonText">PLACEHOLDER</span>
   </div>
 </template>
 
@@ -15,6 +17,13 @@ const buttonText = ref(null);
 
 onMounted(() => {
   const split = new SplitText(buttonText.value, { type: "lines" });
+  gsap.from(split.lines, {
+    y: 40,
+    autoAlpha: 0,
+    stagger: 0.05,
+    duration: 0.6,
+    ease: "back.out(1.7)",
+  });
 });
 </script>
 
